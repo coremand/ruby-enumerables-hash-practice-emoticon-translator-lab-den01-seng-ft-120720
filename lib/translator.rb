1664169ff8fd1ce8224ab2d756fc9a2a54298bdd
Obinna Nwabia <valinevite@gmail.com>
2020-11-24 02:08:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,7 +34,7 @@ def get_japanese_emoticon(file_path,emoticon)
   emoticon_hash = load_library(file_path)
   emoticon_hash.each do |key,value|
     value.map do |key_2,value_2|
-      
+      binding.pry
     end
     
   end
